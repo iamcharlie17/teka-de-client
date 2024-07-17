4f3164bc8ad57b4623bb7b4ecc7d6e8a042af846
Riyad Hosen <riyadhosen@Riyads-MacBook-Air.local>
2024-07-16 23:55:31 -0700

    update user status

--- src/pages/Dashboad/Admin/UserManagement/UserManagement.jsx
+++ src/pages/Dashboad/Admin/UserManagement/UserManagement.jsx
@@ -1,42 +1,73 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { axiosCommon } from "../../../../hooks/useAxiosCommon";
+import toast from "react-hot-toast";
 
 const UserManagement = () => {
   const [users, setUsers] = useState([]);
+  const [control, setControl] = useState(false);
+  const user = JSON.parse(localStorage.getItem("user"));
+
 
   //   const axiosSecure = useAxiosSecure();
 
   useEffect(() => {
-    fetchUsers()
-  }, []);
+    fetchUsers();
+  }, [control]);
 
-  const fetchUsers = (search = '') => {
-    axios.get(`http://localhost:3000/users?search=${search}`)
-    .then(res=> setUsers(res.data))
-  }
+  const fetchUsers = (search = "") => {
+    axios
+      .get(`http://localhost:3000/users?search=${search}`)
+      .then((res) => setUsers(res.data));
+  };
 
   const handleSearch = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const searchValue = e.target.search.value;
-    console.log(searchValue)
-    fetchUsers(searchValue)
-  }
+    fetchUsers(searchValue);
+  };
+
+  const handleStatus = async (value) => {
+    await axiosCommon
+      .put("/update-status", value)
+      .then((res) => {
+        if (res.data.modifiedCount > 0) {
+          toast.success("Status updated!");
+          setControl(!control);
+        }
+      })
+      .catch((err) => {
+        toast.error(`${err.message}`);
+      });
+  };
 
   return (
     <div>
       <div className="overflow-x-auto text-black">
-        <form onSubmit={handleSearch} className="my-4 flex justify-center gap-4">
-    
+        <div className="flex justify-between items-center mx-4">
+          <div>
+            <Link to={"/admin/dashboard"}>
+              <button>Back</button>
+            </Link>
+          </div>
+          <form
+            onSubmit={handleSearch}
+            className="my-4 flex justify-center gap-4 relative"
+          >
             <input
               type="search"
               name="search"
               id="search"
               placeholder="Search by user name"
-              className="border md:w-1/2 px-4 py-2 rounded-lg"
+              className="border w-full px-4 py-2 rounded-lg"
             />
-            <button>Search</button>
-
-        </form>
+            <button>
+              <FaSearch size={25} />
+            </button>
+          </form>
+        </div>
         <table className="table table-xs">
           <thead>
             <tr className="bg-sky-500 text-white">
@@ -49,16 +80,45 @@ const UserManagement = () => {
             </tr>
           </thead>
           <tbody>
-            {users.map((user, idx) => (
-              <tr key={user._id}>
-                <th>{idx + 1}</th>
-                <td>{user?.name}</td>
-                <td>{user?.email}</td>
-                <td>{user?.phoneNumber}</td>
-                <td>{user?.status}</td>
-                <td>{user?.role}</td>
-              </tr>
-            ))}
+            {users
+              .filter((u) => u?.email !== user?.email)
+              .map((u, idx) => (
+                <tr key={u._id}>
+                  <th>{idx + 1}</th>
+                  <td>{u?.name}</td>
+                  <td>{u?.email}</td>
+                  <td>{u?.phoneNumber}</td>
+                  <td>
+                    <div className={` dropdown-top dropdown dropdown-end`}>
+                      <div tabIndex={0} role="button" className=" m-1">
+                        {u?.status}
+                      </div>
+                      <ul
+                        tabIndex={0}
+                        className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow"
+                      >
+                        <li
+                          onClick={() =>
+                            handleStatus({ status: "active", id: u?._id })
+                          }
+                          className="shadow-lg"
+                        >
+                          <a>Active</a>
+                        </li>
+                        <li
+                          onClick={() =>
+                            handleStatus({ status: "block", id: u?._id })
+                          }
+                          className="shadow-lg"
+                        >
+                          <a>Block</a>
+                        </li>
+                      </ul>
+                    </div>
+                  </td>
+                  <td>{u?.role}</td>
+                </tr>
+              ))}
           </tbody>
         </table>
       </div>
